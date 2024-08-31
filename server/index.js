cbc21b1a7aae114758b738fc827da267ffdeb063
Chu-rill <churchillrimamskep@gmail.com>
2024-08-31 11:06:55 +0100

    changed

--- server/index.js
+++ server/index.js
@@ -1,1 +1,20 @@
-console.log("hello");
+class CalorieTracker {
+  constructor(maxCalories) {
+    this.maxCalories = maxCalories;
+    this.currentCalories = 0;
+  }
+  trackCalories(calorieCount) {
+    this.currentCalories += calorieCount;
+    if (this.currentCalories > this.maxCalories) {
+      this.logCaloriesSurplus();
+    }
+  }
+  logCaloriesSurplus() {
+    console.log("Max calories exceeded");
+  }
+}
+
+const calorieTracker = new CalorieTracker(2000);
+calorieTracker.trackCalories(500);
+calorieTracker.trackCalories(1000);
+calorieTracker.trackCalories(700);
