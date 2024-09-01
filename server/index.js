d02a5585aa29022c61bed240b6150672003267e0
Chu-rill <churchillrimamskep@gmail.com>
2024-09-01 21:30:05 +0100

    chnaged

--- server/index.js
+++ server/index.js
@@ -1,20 +1,20 @@
-class CalorieTracker {
-  constructor(maxCalories) {
-    this.maxCalories = maxCalories;
-    this.currentCalories = 0;
-  }
-  trackCalories(calorieCount) {
-    this.currentCalories += calorieCount;
-    if (this.currentCalories > this.maxCalories) {
-      this.logCaloriesSurplus();
-    }
-  }
-  logCaloriesSurplus() {
-    console.log("Max calories exceeded");
-  }
-}
+// class CalorieTracker {
+//   constructor(maxCalories) {
+//     this.maxCalories = maxCalories;
+//     this.currentCalories = 0;
+//   }
+//   trackCalories(calorieCount) {
+//     this.currentCalories += calorieCount;
+//     if (this.currentCalories > this.maxCalories) {
+//       this.logCaloriesSurplus();
+//     }
+//   }
+//   logCaloriesSurplus() {
+//     console.log("Max calories exceeded");
+//   }
+// }
 
-const calorieTracker = new CalorieTracker(2000);
-calorieTracker.trackCalories(500);
-calorieTracker.trackCalories(1000);
-calorieTracker.trackCalories(700);
+// const calorieTracker = new CalorieTracker(2000);
+// calorieTracker.trackCalories(500);
+// calorieTracker.trackCalories(1000);
+// calorieTracker.trackCalories(700);
