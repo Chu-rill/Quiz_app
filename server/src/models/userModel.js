ec030279245031c4d586269e9dd6dc861117febe
Chu-rill <churchillrimamskep@gmail.com>
2024-09-03 20:48:00 +0100

    added the user model

---
+++ server/src/models/userModel.js
@@ -0,0 +1,10 @@
+const mongoose = require("mongoose");
+
+const UserSchema = new mongoose.Schema({
+  name: { type: String, required: true },
+  email: { type: String, required: true, unique: true },
+  password: { type: String, required: true },
+  role: { type: String, default: "user" }, // 'admin' or 'user'
+});
+
+module.exports = mongoose.model("User", UserSchema);
