3257aa9211c799b3f618d54a0bb7f7cffd0a10c8
Chu-rill <churchillrimamskep@gmail.com>
2024-09-06 16:59:38 +0100

    added a randomly generated user id

--- server/src/models/userModel.js
+++ server/src/models/userModel.js
@@ -1,10 +1,14 @@
 const mongoose = require("mongoose");
+const { v4: uuidv4 } = require("uuid"); // Import the UUID function
 
 const UserSchema = new mongoose.Schema({
   name: { type: String, required: true },
-  email: { type: String, required: true, unique: true },
   password: { type: String, required: true },
-  role: { type: String, default: "user" }, // 'admin' or 'user'
+  userId: {
+    type: String,
+    default: uuidv4, // Generates a random UUID for each user
+    unique: true, // Ensure that the random ID is unique
+  },
 });
 
 module.exports = mongoose.model("User", UserSchema);
